625683db5c74ad4dfeacf27c3f5d2c9a46d309a9
ammrat13 <ammrat13@gmail.com>
2018-01-13 18:26:47 -0600

    Started IntelliJ Project

---
+++ src/com/ammrat13/javagame/Main.java
@@ -0,0 +1,13 @@
+package com.ammrat13.javagame;
+
+/**
+ * This class is responsible for starting the game.
+ *
+ * @author Ammar Ratnani
+ */
+
+public class Main {
+	public static void main(String[] args){
+	
+	}
+}
