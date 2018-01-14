696fa2f8035dbe40b255ab9c4749079a3f6bf203
ammrat13 <ammrat13@gmail.com>
2018-01-13 19:53:12 -0600

    Added sound capability

--- src/com/ammrat13/javagame/GameManager.java
+++ src/com/ammrat13/javagame/GameManager.java
@@ -4,8 +4,14 @@ import com.ammrat13.javagame.gamescenes.GamePlayScene;
 
 import java.awt.*;
 import java.awt.event.*;
+
+import java.io.*;
+
 import java.util.*;
 import java.util.Timer;
+
+import javax.sound.sampled.*;
+
 import javax.swing.*;
 
 /**
@@ -19,6 +25,10 @@ public class GameManager extends JPanel implements KeyListener {
 	
 	public final int WIDTH;
 	public final int HEIGHT;
+	
+	// The sound playing
+	private Clip clip;
+	public static final String BG_SOUND = "MainMusic.wav";
 	
 	// Map of all the scenes we have
 	public Map<String, GameScene> gss;
@@ -45,13 +55,59 @@ public class GameManager extends JPanel implements KeyListener {
 			}
 		}, 0, 20);
 		
-		// Play music
+		setSound(BG_SOUND);
 	}
+	
+	/**
+	 * Sets the active scene.
+	 *
+	 * @param ags The name of the scene to make active
+	 */
 	
 	public void setActive(String ags){
 		if(active != null)
 			active.stop();
 		active = gss.get(ags);
+	}
+	
+	/**
+	 * Sets the background sound to play continuously
+	 *
+	 * @param soundPath The path of the sound to play
+	 * @return The length of the sound
+	 */
+	
+	public int setSound(String soundPath){
+		// Source: https://www.geeksforgeeks.org/play-audio-file-using-java/
+		try {
+			AudioInputStream ais = AudioSystem.getAudioInputStream(new File(soundPath).getAbsoluteFile());
+			
+			clip = AudioSystem.getClip();
+			clip.open(ais);
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
+			
+			return (int) (1000 * ((double) ais.getFrameLength() / ais.getFormat().getFrameRate()));
+		} catch(UnsupportedAudioFileException | IOException | LineUnavailableException e){
+			e.printStackTrace();
+		}
+		
+		return 0;
+	}
+	
+	/**
+	 * Interrupts the background sound and plays one loop of the sound provided.
+	 *
+	 * @param soundPath The path of the sound to play
+	 */
+	
+	public void interruptSound(String soundPath){
+		Timer s = new Timer();
+		s.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				setSound(BG_SOUND);
+			}
+		}, setSound(soundPath));
 	}
 	
 	private void update(){
