4dbe23ca24fe2fb14de6fab4383708ad45864e72
ammrat13 <ammrat13@gmail.com>
2018-01-13 20:30:55 -0600

    Added a test object

---
+++ src/com/ammrat13/javagame/objects/TestObject.java
@@ -0,0 +1,46 @@
+package com.ammrat13.javagame.objects;
+
+import com.ammrat13.javagame.gamescenes.GamePlaySceneObject;
+import com.ammrat13.javagame.util.Vec;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * An object that does nothing. For testing purposes.
+ *
+ * @author Ammar Ratnani
+ */
+
+public class TestObject implements GamePlaySceneObject {
+	
+	private Vec pos;
+	
+	public TestObject(double x, double y){
+		pos = new Vec(x,y);
+	}
+	
+	@Override
+	public void update(){}
+	
+	@Override
+	public void keyDown(int kCode){}
+	
+	@Override
+	public Vec getPos(){return pos;}
+	
+	@Override
+	public BufferedImage render(){
+		BufferedImage ret = new BufferedImage(10, 10, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2d = (Graphics2D) ret.getGraphics();
+		
+		g2d.setColor(Color.WHITE);
+		g2d.fillRect(0,0,10,10);
+		
+		return ret;
+	}
+	
+	@Override
+	public Vec renderOffset(){return new Vec(-5,-5);}
+	
+}
